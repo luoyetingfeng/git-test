f961bc831251184f15741d7e9cd065871880f645
lizhanlei <lizhanlei@qq.com>
2019-09-27 00:47:34 +0800

    把本地工程添加到2dev个文件

---
+++ git-test-project/src/com/itheima/git/SecondClass.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("dierge");
+        System.out.println("dierge");
+        System.out.println("dierge");
+        System.out.println("dierge");
+    }
+}
